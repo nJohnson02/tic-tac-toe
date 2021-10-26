458eaa845fd7c23d84f457bb4af7a768355eaf05
dapp3rduck <n.johnson01@protonmail.ch>
2021-10-26 08:58:01 -0700

    more pseudocode

--- main.py
+++ main.py
@@ -3,3 +3,14 @@
 #Ai method
 
 #Game engine (main program)
+    #create a game board (2d array)
+    
+    #take in game parameters
+
+    #start the game (while loop)
+        #draw board
+        #player1 makes move
+        #check for win
+        #draw board
+        #player2 makes move
+        #check for win

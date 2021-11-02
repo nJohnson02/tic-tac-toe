f5754ff3fef880d1a52f83bf165b95a7c96184e2
dapp3rduck <n.johnson01@protonmail.ch>
2021-11-02 08:35:43 -0700

    multiple files?

--- core.py
+++ core.py
@@ -2,10 +2,14 @@
     #termcolor / matplotlib / pygame?
 
 #Draw Method
+import functions.graphics as graphics
 
 #check for win method
+import functions.checks as checks
 
 #Ai method
+import functions.ai as ai
+
 
 #Game engine (main program)
     #create a game board (2d array)
